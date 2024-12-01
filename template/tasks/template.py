e5d33742e5c5edae3020cd750724b515083db4d6
Denmads <43139465+Denmads@users.noreply.github.com>
2024-12-01 14:39:38 +0100

    Added template folder

---
+++ template/tasks/template.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+# //////////////////// PARSING & TYPES /////////////////////////
+
+def parse_input(data: str, part: str) -> Any:
+    pass
+
+
+# //////////////////// PARTS /////////////////////////
+
+def run_a(data: Any):
+    pass
+
+def run_b(data: Any):
+    pass
